920d784ebe8acfc176c005c0dbf0a60ef3d3c149
SUSHMITHA-GANGADHAR <sushmithagsushmithag7@gmail.com>
2024-09-06 23:53:20 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,3 +1,8 @@
+'''Build a decision tree classifier to predict whether a customer will purchase a product or service based on 
+their demographic and behavioral data. Use a dataset such as the Bank Marketing dataset from 
+the UCI Machine Learning Repository.'''
+
+
 # Required Libraries
 import matplotlib.pyplot as plt
 import seaborn as sns
